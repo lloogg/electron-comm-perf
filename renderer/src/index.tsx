d88f068e4522089faf33bc60c05c7f510a63edbb
weixuyang <weixuyang@digiproto.com>
2022-01-30 11:58:55 +0800

    ..

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -1,8 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import ReactDOM, { createPortal } from 'react-dom';
+const Apple = () => {
+  return <div>apple</div>;
+};
+
 const App = () => {
   let externalWindow = null;
   let containerEl = document.createElement('div');
+  const [show, setShow] = useState(false);
   useEffect(() => {
     return () => {
       externalWindow.close();
@@ -12,16 +17,15 @@ const App = () => {
     <div
       onClick={() => {
         externalWindow = window.open('about:blank', 'windowPortal');
-        externalWindow.document.body.appendChild(containerEl);
         createPortal(<Apple />, containerEl);
+
+        externalWindow.document.body.appendChild(containerEl);
       }}
     >
       hello world
+      {/* {show && } */}
+      <Apple />
     </div>
   );
 };
 ReactDOM.render(<App />, document.getElementById('root'));
-
-const Apple = () => {
-  return <div>apple</div>;
-};
